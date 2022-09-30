48047397836bb617588110ad02a487031bd51e38
erick.layson <erick.layson@gmail.com>
2022-10-01 03:02:23 +0800

    20221001 0302 Added changes to code

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -75,7 +75,7 @@ export default function ProductCard({ productProp }) {
 
     return (
 
-        <div className="p-5 col-md-1 col-1">
+        <div className="p-5">
             
             <Card>
                     
