0be4d52c545fa67b9834c6ea07a2a95567e748d3
erick.layson <erick.layson@gmail.com>
2022-10-31 18:04:59 +0800

    20221031 1804 add changes to code

--- src/pages/ProductView.js
+++ src/pages/ProductView.js
@@ -2,11 +2,31 @@ import { useState, useEffect, useContext } from "react";
 
 import { Container, Card, Button, Row, Col, Form } from "react-bootstrap";
 import { useParams, useNavigate, Link } from "react-router-dom";
+
+
+
 import Swal from "sweetalert2";
 
 import UserContext from "../UserContext";
 
+const cartFromLocalStorage = JSON.parse(localStorage.getItem('cart') || "[]");
+
+let fromfield = [];
+
 export default function ProductView() {
+
+
+
+    /* ADD TO CART REQUIREMENTS */
+
+
+
+    const [cart, setCart] = useState(cartFromLocalStorage);
+
+    useEffect(() => {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    }, [cart]);
+
 
     const { user } = useContext(UserContext);
 
@@ -18,9 +38,10 @@ export default function ProductView() {
 
 
 
+
     // Create state hooks to capture the information of a specific course and display it in our application.
     const [name, setName] = useState('');
-    /* const [description, setDescription] = useState(''); */
+    
     const [price, setPrice] = useState(0);
     const [stocks, setStocks] = useState(0);
     const [image, setImage] = useState(0);
@@ -29,30 +50,76 @@ export default function ProductView() {
     const [quantity, setQuantity] = useState('');
 
 
-    // const [seats, setSeats] = useState(30)
-    //const [count, setCount] = useState(0);
-    //const [disabled, setDisabled] = useState(false);
+
 
     useEffect(() => {
-        //console.log(productId);
-
         fetch(`${process.env.REACT_APP_API_URL}/products/${productId}`)
             .then(res => res.json())
             .then(data => {
-
-
                 setName(data.name);
-                /*  setDescription(data.description); */
                 setPrice(data.price);
                 setStocks(data.stocks);
                 setImage(data.image);
-
             });
 
     }, [productId])
 
-    /* console.log(productId);
-    console.log(productId); */
+    
+
+
+    /* ADDCART FUNCTION START */
+    const addcart = (productId) => {
+        if (quantity < 1) {
+            alert("Quantity can't be lower than 1!");
+        }
+        else if (fromfield.length === 0){
+            fromfield.push({
+                cartId: productId,
+                cartName: name,
+                cartQuantity: quantity,
+                cartImage: image,
+                cartPrice: price,
+                cartSubtotal: quantity * price,
+            });
+            localStorage.setItem(`cart`, JSON.stringify([...fromfield]));
+            alert(quantity + " item/s added to cart!");
+            navigate("/products");
+        }
+        else {
+            let fromfieldend = fromfield.length;
+                for (let i = 0 ; i < fromfieldend ; i++){
+                    if(fromfield[i].cartId === productId){
+                        fromfield[i].cartId = productId;
+                        fromfield[i].cartName = name;
+                        fromfield[i].cartQuantity = parseInt(fromfield[i].cartQuantity) + parseInt(quantity);
+                        fromfield[i].cartImage = image;
+                        fromfield[i].cartSubtotal = fromfield[i].cartQuantity * price;
+                        localStorage.setItem(`cart`, JSON.stringify([fromfield[i]]));
+                        alert(quantity + " item/s added to cart!");
+                        navigate("/products");
+                    }
+                    else {
+                        //alert("cart full")
+                        fromfield.push({
+                            cartId: productId,
+                            cartName: name,
+                            cartQuantity: quantity,
+                            cartImage: image,
+                            cartPrice: price,
+                            cartSubtotal: quantity * price,
+                        });
+                        alert(quantity + " item/s added to cart!");
+                        localStorage.setItem(`cart`, JSON.stringify([...fromfield]));
+                        navigate("/products");
+                    }
+                }
+            } 
+    }
+
+
+
+    /* ADDCART FUNCTION END */
+
     const checkout = (productId) => {
 
 
@@ -106,46 +173,7 @@ export default function ProductView() {
     }
 
 
-    /* 
-        function quantityfier() {
-             if(stocks<=0)
-            {
-                 alert("No more stocks!"); 
-            }
-            else
-            {
-                setQuantity(quantity+1);
-            }
-            
-    
-        }
-    
-    
-    
-        function quantityfiertwo() {
-            if ( quantity <= 0) {
-                alert("Quantity can't be lower than 1!");
-            }
-            else {
-                setQuantity(parseInt(quantity)  + 1);
-            }
-    
-        }  */
-
-   /*    useEffect(() => {
-         
-  
-          if (quantity <= 0) {
-              //setDisabled(true);
-              alert("Quantity can't be lower than 1.");
-          }
-          else if(quantity >= stocks)
-          {
-              alert("No more stocks.");
-          }
-      }, [quantity]) */
-
-
+ 
     return (
         <Container className="mt-5 mb-5">
             <Row>
@@ -181,13 +209,21 @@ export default function ProductView() {
                                                 />
                                             </Form>
                                         </Card.Text>
+                                        
+                                        <Card.Text>
                                         {/*  <Button className="btn btn-dark ms-2" variant="primary" onClick={quantityfiertwo}>-</Button> */}
                                         <Button variant="primary" size="lg" onClick={() => checkout(productId)}>Checkout</Button>
+                                        </Card.Text>
 
+                                        <Card.Text>
+                                        {/* <Button onClick={() => handleClick(productId)}>Add to Cart</Button> */}
+                                            {/* <Button variant="primary" size="lg" onClick={() => addItem(productId)}>Add to Cart</Button>
+                                        </Card.Text> */}
+                                            <Button variant="primary" size="lg" onClick={() => addcart(productId)}>Add to Cart</Button>
+                                        </Card.Text>
                                     </>
                                     :
-                                    <Button as={Link} to="/login" variant="success" size="lg">Login to Checkout Item</Button>
-                            }
+                                    <Button as={Link} to="/login" variant="success" size="lg">Login to Checkout Item</Button>}
                         </Card.Body>
                     </Card>
                 </Col>
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -3,16 +3,33 @@ import { useEffect, useState, useContext } from "react";
 import { Navigate } from "react-router-dom";
 
 import ProductCard from "../components/ProductCard";
+
+//import Cart from './Cart';
+//import cart from "../pages/ProductView";
+
 import UserContext from "../UserContext";
 import "./Products.css";
 
+
+
+
+
+
+
+
 export default function Products() {
+
+
+
+const [cart, setCart] = useState([]);
 
     const { user } = useContext(UserContext);
 
     // couses state that will be used to store the products retrieve in the database.
 
     const [Products, setProducts] = useState([]);
+    
+
 
     useEffect(() => {
         // Will retrieve all the active products
@@ -38,11 +55,19 @@ export default function Products() {
             <>
                 
                 <h1 className="text-center">Products</h1>
+                <div className="text-center">
+                    <button>
+                      Cart
+                    </button>
+                     
+                </div>
                 <div className="wrapper mb-5 mt-5">
 
 
 
                     {Products}
+
+
 
                 </div>
             </>
